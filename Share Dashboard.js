a8fad1eb53f96576f40ac628c826630e0787212f
John Sanchez <john.sanchez@ALLEGIANCE.LOCAL>
2020-02-27 14:14:58 -0600

    Remove self-calling function from Share DB script

--- Share Dashboard.js
+++ Share Dashboard.js
@@ -1,11 +1,12 @@
-(async function () { 
+function sleep(ms) { 
+    return new Promise(resolve => setTimeout(resolve, ms)); 
+}
+async function shareDashboard () { 
     $('.js-menuShareDashboard>a').click(); 
     await sleep(3000); $('.sb-moveAll')[0].click(); 
     $('.js-reportingDetailViewButtons .primaryBtn').click(); 
     $('.js-menuLoadReport a').click(); 
     await sleep(1000); 
     $('.reportName').click(); 
-})(); 
-function sleep(ms) { 
-    return new Promise(resolve => setTimeout(resolve, ms)); 
 }
+shareDashboard(); 
